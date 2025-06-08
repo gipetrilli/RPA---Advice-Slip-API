d316e8ec01f3ed1f63227b398ca6a68b3cb0c9a8
giovannapetrilli <giovanna.venditti@aluno.faculdadeimpacta.com.br>
2025-06-08 19:15:04 -0300

    processamento e armazenamento

--- conselhos.py
+++ conselhos.py
@@ -1,6 +1,7 @@
 import requests
 import sqlite3
 import time
+import re
 
 conn = sqlite3.connect("projeto_rpa.db")
 cursor = conn.cursor()
@@ -9,6 +10,15 @@ cursor.execute('''
 CREATE TABLE IF NOT EXISTS conselhos (
     id INTEGER PRIMARY KEY,
     texto TEXT NOT NULL
+)
+''')
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS dados_processados (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id_conselho INTEGER,
+    palavras_longas TEXT,
+    FOREIGN KEY(id_conselho) REFERENCES conselhos(id)
 )
 ''')
 conn.commit()
@@ -21,21 +31,39 @@ def buscar_conselho():
         data = response.json()
         return data['slip']['id'], data['slip']['advice']
     else:
-        print("Erro ao acessar API.")
-        return None, None
+        return new_func()
+
+def new_func():
+
+    return None, None
 
 def inserir_conselho(advice_id, advice_text):
     try:
         cursor.execute("INSERT OR IGNORE INTO conselhos (id, texto) VALUES (?, ?)", (advice_id, advice_text))
         conn.commit()
-        print(f"Conselho inserido: {advice_id} - {advice_text}")
+        print(f"Conselho inserido: {advice_id}")
     except Exception as e:
         print("Erro ao inserir:", e)
+
+def extrair_palavras(texto):
+    return ', '.join(re.findall(r'\b\w{7,}\b', texto))
 
 for i in range(10):
     aid, texto = buscar_conselho()
     if aid and texto:
         inserir_conselho(aid, texto)
-    time.sleep(1.5)  
+    time.sleep(1.5)
+
+cursor.execute("SELECT id, texto FROM conselhos")
+conselhos = cursor.fetchall()
+
+for cid, texto in conselhos:
+    palavras = extrair_palavras(texto)
+    cursor.execute('''
+        INSERT INTO dados_processados (id_conselho, palavras_longas)
+        VALUES (?, ?)
+    ''', (cid, palavras))
+    conn.commit()
+    print(f"Processado conselho {cid}: {palavras}")
 
 conn.close()
